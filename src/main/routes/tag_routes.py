cf9b2b287f4b7b36ddbd54ad679041ea824ff6f1
pauloabraao <abraaoteles@alu.ufc.br>
2024-02-09 16:53:29 -0300

    feat: Implementing Validator

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -4,13 +4,17 @@ from src.views.tag_creator_view import TagCreatorView
 
 from src.errrors.error_handler import handle_errors
 
+from src.validators.tag_creator_validator import tag_creator_validator
+
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
     response = None
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
+        
         http_request = HttpRequest(body=request.json)
         response = tag_creator_view.validate_and_create(http_request)
     except Exception as exception:
